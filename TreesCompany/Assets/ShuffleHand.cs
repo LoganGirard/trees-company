6a07ba4c88e81a074cc1e1e2640f56ff4c609c35
Pablo Hernandez Cepeda <pablohrdz@gmail.com>
2019-05-11 21:01:26 -0700

    Shuffle Hand (incomplete), Card Prefabs, GameManager

---
+++ TreesCompany/Assets/ShuffleHand.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ShuffleHand : MonoBehaviour
+{
+    public bool ShouldShuffle = false;
+
+    public GameObject[] PoolOfCards;
+
+    private const int CARDS_IN_HAND = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ShouldShuffle)
+        {
+            foreach(Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+
+            List<GameObject> hand = GetRandomHandWithoutReplacement(CARDS_IN_HAND);
+
+            foreach(GameObject card in hand)
+            {
+                Instantiate(card);
+            }
+        }
+    }
+
+    List<GameObject> GetRandomHandWithoutReplacement(int numCards)
+    {
+        List<GameObject> pool = new List<GameObject>(PoolOfCards);
+        List<GameObject> hand = new List<GameObject>();
+
+        for(int i = 0; i < numCards; i++)
+        {
+            int randomIndex = Random.Range(0, pool.Count);
+            GameObject card = PoolOfCards[randomIndex];
+
+            hand.Add(card);
+            pool.RemoveAt(randomIndex);
+        }
+
+        return hand;
+    } 
+}
