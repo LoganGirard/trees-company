2331c1085583b451f9a2087c5f594d9879353946
Jorge Gaspar <jorge.gaspar.lira@gmail.com>
2019-05-11 20:39:48 -0700

    Zooming is possible now, We can scroll with the left click, and tiles are now clickable

--- TreesCompany/Assets/Clicker.cs
+++ TreesCompany/Assets/Clicker.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Clicker : MonoBehaviour
 {
+    Camera Camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera = GetComponent<Camera>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+                // the object identified by hit.transform was clicked
+                // do whatever you want
+            }
+        }
     }
 }
